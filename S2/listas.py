1db364a8849a3808d15443ad5e73c84cb5b493e9
Luis Dominguez <l.dominguez.aldana@gmail.com>
2021-06-27 19:58:47 -0500

    S2 Part 1

---
+++ S2/listas.py
@@ -0,0 +1,12 @@
+lista_vacia = []
+lista_vacia2 = list()
+
+#Lista Poblada
+list1 = [100,200,300,400,500]
+print(list1)
+
+#Operaciones
+
+#Lectura de indice
+print("list1 en posición 2:")
+print(list1[1])
